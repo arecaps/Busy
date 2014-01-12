216ffc487626c2a3a60af0439bddc16015c778ef
arecaps <arecaplan@gmail.com>
2014-01-12 14:49:24 -0500

    Fixed the main method

--- DeckOfCards/src/deckofcards/DeckOfCards.java
+++ DeckOfCards/src/deckofcards/DeckOfCards.java
@@ -1,15 +1,21 @@
-
 package deckofcards;
 
+import java.util.Scanner;
+
 public class DeckOfCards {
-    
-            
+
     public static void main(String[] args) {
-        //Deck deck = new Deck();
-        //deck.printDeck();
-        
-        Blackjack blackjack = new Blackjack();
-        
+
+        Deck deck = new Deck();
+        deck.printDeck();
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Do you want to play Blackjack?  y/n");
+        String choice = sc.next();
+        if (choice.equalsIgnoreCase("y")) {
+            Blackjack blackjack = new Blackjack();
+        }  else {
+            System.out.println("Okay, maybe a different time.");
+        }
     }
-    
-}
+    }
