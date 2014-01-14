833ecc02ea9617b7c005259414be3b5c932a9ae5
arecaps <arecaplan@gmail.com>
2014-01-14 18:44:54 -0500

    Makes each product

---
+++ Order/src/order/Product.java
@@ -0,0 +1,33 @@
+package order;
+
+public class Product {
+
+    private String itemCode;
+    private String description;
+    private double price;
+    private boolean taxable;
+
+    public Product(String itemCode, String description, double price, boolean taxable) {
+        this.itemCode = itemCode;
+        this.description = description;
+        this.price = price;
+        this.taxable = taxable;
+    }
+    public String getProduct(){
+        return description;
+    }
+    public double getPrice(){
+        return price;
+    }
+    public boolean getTaxable(){
+        return taxable;
+    }
+
+    @Override
+    public String toString() {
+        String item = "Code:\t\t" + itemCode
+                + "\nDescription:\t" + description
+                + "\nPrice:\t\t" + price + "\n";
+        return item;
+    }
+}
