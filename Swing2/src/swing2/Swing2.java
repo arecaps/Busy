7ea49e14fbebfce0b6c99aea4e4b032c662eb425
arecaps <arecaplan@gmail.com>
2014-01-27 21:07:01 -0500

    First Swing

---
+++ Swing2/src/swing2/Swing2.java
@@ -0,0 +1,66 @@
+package swing2;
+
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
+import javax.swing.*;
+
+public class Swing2 {
+
+    int i = 0;
+    int j = 0;
+
+    public Swing2() {
+        JFrame frame = new JFrame();
+        frame.setBounds(100, 100, 300, 200);
+        frame.addMouseListener(new MouseListener() {
+
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                System.out.println("Mouse Clicked");
+            }
+
+            @Override
+            public void mousePressed(MouseEvent e) {
+                System.out.println("Mouse Pressed");
+            }
+
+            @Override
+            public void mouseReleased(MouseEvent e) {
+                System.out.println("Mouse Released");
+            }
+
+            @Override
+            public void mouseEntered(MouseEvent e) {
+                System.out.println("Mouse Entered");
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+                System.out.println("Mouse Exited");
+            }
+
+        });
+        frame.addMouseMotionListener(new MouseMotionListener() {
+
+            @Override
+            public void mouseDragged(MouseEvent e) {
+                j++;
+                System.out.println("Mouse Dragged" + j + " times");
+            }
+
+            @Override
+            public void mouseMoved(MouseEvent e) {
+                i++;
+                System.out.println("Mouse Moved " + i + " times");
+            }
+        });
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new Swing2();
+    }
+
+}
