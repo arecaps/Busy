2cf0cc46ae1c04284a8043ee3de18ba094c8303d
arecaps <arecaplan@gmail.com>
2014-02-04 21:22:54 -0500

    Stop complaining

---
+++ ComplaintDept/src/complaintdept/ComplaintDept.java
@@ -0,0 +1,72 @@
+package complaintdept;
+
+import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionListener;
+import javax.swing.*;
+
+public class ComplaintDept extends JFrame {
+
+    int x = 275;
+    int y = 300;
+
+    public void runAway(JButton button, int a, int b) {
+        if (a < 250) {
+            a += 275;
+        } else if (a > 350) {
+            a -= 275;
+        } else {
+            a += 150;
+        }
+        if (b < 300) {
+            b += 300;
+        } else if (b > 400) {
+            b -= 300;
+        } else {
+            b += 150;
+        }
+        button.setLocation(a, b);
+        x = a;
+        y = b;
+        
+    }
+
+    public ComplaintDept() {
+        setSize(700, 700);
+        this.setTitle("Complaint Department");
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        JPanel board = new JPanel();
+        board.setLayout(null);
+        final JButton button = new JButton("Press to complain");
+        button.setSize(new Dimension(125, 75));        
+        board.addMouseMotionListener(new MouseMotionListener() {
+
+            @Override
+            public void mouseDragged(MouseEvent e) {
+            }
+
+            @Override
+            public void mouseMoved(MouseEvent e) {
+                int a = e.getX();
+                int b = e.getY();
+                System.out.println(a + ", " + b);
+                if(((a > x - 30 && a < x + 125 )|| (a < x + 155 && a > x)) 
+                        && ((b > y - 30 && b < y  + 50)|| (b < y + 105 && b > y))) {
+                    runAway(button, a, b);
+                }
+
+            }
+        });
+        add(board);
+        board.add(button);
+        button.setLocation(x, y);
+        setResizable(false);
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new ComplaintDept();
+    }
+
+}
