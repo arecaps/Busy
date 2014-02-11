83ac504af31051c1c4df2e442e60ed278d4b3acf
arecaps <arecaplan@gmail.com>
2014-02-11 18:10:39 -0500

    Added new Thread

--- Tetris/src/tetris/Tetris.java
+++ Tetris/src/tetris/Tetris.java
@@ -1,33 +1,50 @@
 package tetris;
 
 import java.awt.*;
+import java.awt.event.*;
 import java.util.ArrayList;
 import javax.swing.*;
 
 public class Tetris extends JFrame {
 
     public ArrayList<Piece> pieces = new ArrayList<>();
+    private Color color= new Color(238, 238, 238);
+    JPanel board;
     int shape = 5;
     int j = 0;
+    
 
-    public void startGame() {
-       while (shape < 300) {
+    public void dropPiece() {
+        while (shape < 300) {
+            for(Piece piece : pieces){
+                if(piece.getBackground().equals(Color.red)){
+                    piece.setBackground(color);
+                }
+            }
             for (int i = 1; i < 4; i++) {
                 pieces.get(shape).setBackground(Color.red);
                 shape++;
             }
             shape += 13;
             pieces.get(shape).setBackground(Color.red);
-            repaint();
-            this.setTitle("Sleeping  : " + j);
-            j++;
+            board.repaint();
             try {
-                Thread.sleep(300);
+                Thread.sleep(750);
             } catch (InterruptedException ex) {
                 return;
             }
-            System.out.println("Waking up");
         }
+    }
+
+    public void startGame() {
+        Thread animationThread = new Thread(
+                new Runnable() {
+                    public void run() {
+                        dropPiece();
+                    }
+                }
+        );
+        animationThread.start();
     }
 
     public void addButtons(JPanel board) {
@@ -51,10 +68,10 @@ public class Tetris extends JFrame {
         Controls controlPanel = new Controls(Tetris.this, controls);
         controlPanel.startButton();
         add(controls, BorderLayout.SOUTH);
-        JPanel board = new JPanel();
+        board = new JPanel();
         board.setLayout(new GridLayout(20, 16, 1, 1));
         addButtons(board);
-        //System.out.println(board.getBackground());
+        
         add(board);
         setResizable(false);
         setVisible(true);
